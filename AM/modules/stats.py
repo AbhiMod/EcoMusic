5fc4610f463c9d9e204b5b7b8f1d6b9ee9e376b5
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-12-17 16:41:30 +0530

    Update stats.py

--- AM/modules/stats.py
+++ AM/modules/stats.py
@@ -10,14 +10,20 @@ from pyrogram.types import (
     InlineKeyboardButton,
 )
 
-
-
-# ------------------------------------------------------------------------------- #
-
-
-
-
-
+AM_PIC = [
+    "https://telegra.ph/file/7c25ef427c9f3cded5577.jpg",
+    "https://telegra.ph/file/625d235cc0a22fb8525b5.jpg",
+    "https://telegra.ph/file/1c62254d59baf7f968ba7.jpg",
+    "https://telegra.ph/file/7a0553bd4664486ab3008.jpg",
+    "https://telegra.ph/file/7b4dfa606e6f23961d30e.jpg",
+    "https://telegra.ph/file/2773dec98d87b8562618c.jpg",
+    "https://telegra.ph/file/80353d02e0368b71d2666.jpg",
+    "https://telegra.ph/file/6e5331dc4bef87464ea1c.jpg",
+    "https://telegra.ph/file/199a2e44cb8e77bb21b34.jpg",
+    "https://telegra.ph/file/8371bcd8952d089f9ec05.jpg",
+    "https://telegra.ph/file/f970e559dd1bb96fced1a.jpg",
+    "https://telegra.ph/file/59a305f8ce0c4e85949cc.jpg"
+]
 
 
 @AM.on_message(group=10)
@@ -48,7 +54,8 @@ async def chat_watcher_func(_, message):
 async def stats(cli: Client, message: Message):
     users = len(await get_users())
     chats = len(await get_chats())
-    await message.reply_photo("https://graph.org/file/2eb489e499193ad8f0f24.jpg",
+    await message.reply_photo(
+        photo=random.choice(AM_PIC),
         caption=f"""**ᴛᴏᴛᴀʟ sᴛᴀᴛs ᴏғ** {(await cli.get_me()).mention} :
 
 ➻ ᴄʜᴀᴛs : {chats}
