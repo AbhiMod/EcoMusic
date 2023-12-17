a0d0651e0957792af76c88a4f86194f727f2d57b
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-12-18 00:11:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,5 +12,5 @@ MONGO_URL = getenv("MONGO_URL", "mongodb+srv://EcoMusic:WeLjC5Tml2Hj69Kb@ecomusi
 SESSION_STRING = getenv("SESSION_STRING", "BAAkcM6FsqSIz0of_hYsJUDqroCGnwLIcQnhVbXqaLN1fjGJTmaa2xxteWE6oJ_eBRkm7hKxsMDhZUjQpPs8JDz51vs8svXppAAqvPMOShPpubtBORz6pkvXYRP1LS14Yn1IHRqO3tvzFqXIR_u9lP5DbUoVUHGh4BqnDXl0hsmHhpgBRPgQfVmDtfJDaeByLTmiC4sfG89VzPinN_V4oUw4r0FbFDYHWvzUmuf1188LMBQYddMaHFBUOa2fk4VSlzpkGBcuS-6iOc0dxtBujEoi7G5w_q8bWwawA_iSlCyXHhbmJlDFcIzrEtFsThPTiHVQJiqkUSb5U94O7aNWGWDfAAAAAY_t9d4A")
 CHANNEL = getenv("CHANNEL", "https://t.me/AMBOTYT")
 SUPPORT = getenv("SUPPORT", "https://t.me/AM_YTSUPPORT")
-OWNER_USERNAME = getenv("OWNER_USERNAME","AM_YTBOTT")
+OWNER_USERNAME = getenv("OWNER_USERNAME","Sanam_King")
 PING_PIC = ["https://telegra.ph/file/7c25ef427c9f3cded5577.jpg","https://telegra.ph/file/625d235cc0a22fb8525b5.jpg","https://telegra.ph/file/1c62254d59baf7f968ba7.jpg","https://telegra.ph/file/7a0553bd4664486ab3008.jpg","https://telegra.ph/file/7b4dfa606e6f23961d30e.jpg", "https://telegra.ph/file/2773dec98d87b8562618c.jpg", "https://telegra.ph/file/80353d02e0368b71d2666.jpg","https://telegra.ph/file/6e5331dc4bef87464ea1c.jpg","https://telegra.ph/file/199a2e44cb8e77bb21b34.jpg","https://telegra.ph/file/8371bcd8952d089f9ec05.jpg","https://telegra.ph/file/f970e559dd1bb96fced1a.jpg","https://telegra.ph/file/59a305f8ce0c4e85949cc.jpg"]
